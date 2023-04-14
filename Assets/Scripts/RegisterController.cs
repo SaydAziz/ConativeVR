13505101dda9282b2d57e9dfe19e56fb2c261027
Aziz Saydaminov <sayd.aziz02@gmail.com>
2023-04-14 11:53:32 -0500

    Adds pushable button on register.

---
+++ Assets/Scripts/RegisterController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterController : MonoBehaviour
+{
+    [SerializeField] private string code = "12345";
+
+}
