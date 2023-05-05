df6fcbe2eb6a6f3171db37b9ebc72a1dc954fded
Hannah Hughes <hannahhughes82@gmail.com>
2023-05-04 19:46:46 -0500

    UI added
    added UI script and all the UI for the player and the instruction UI

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log(Input.inputString);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+
+    }
+}
