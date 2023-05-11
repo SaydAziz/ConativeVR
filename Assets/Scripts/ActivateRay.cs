19ceda19ba88d86ccbcd137505fe8af086f33408
Hannah Hughes <hannahhughes82@gmail.com>
2023-05-11 14:06:56 -0500

    Colliders/Ray
    Added the ray so the player can interact with the menu also added colliders to objects in the environment and made sure everything is interactable

---
+++ Assets/Scripts/ActivateRay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActivateRay : MonoBehaviour
+{
+    public GameObject leftGrabRay;
+    public GameObject rightGrabRay;
+
+    public XRDirectInteractor leftDirectGrab;
+    public XRDirectInteractor rightDirectGrab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //grab ray only activates when holding nothing
+        leftGrabRay.SetActive(leftDirectGrab.interactablesSelected.Count == 0);
+        rightGrabRay.SetActive(rightDirectGrab.interactablesSelected.Count == 0);
+    }
+}
